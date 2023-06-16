0eedc6d84093fa2944bd97982358a659646f5225
SoufianeAziz <azizsoufiane04@gmail.com>
2023-06-16 08:11:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/**
- *main - prints a given phrase
- *return: 0 if exited properly, non-zero otherwise
-*/
 
-int main(void)
+/**
+ * main - Entry point
+ *
+ *Description: A C program that prints with put function 
+ *
+ * Return: Always 0 (Success)
+*/ 
+int main()
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+puts("\"Programming is like building a multilingual puzzle");	
+return (0);
 }
