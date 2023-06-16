b6067c1f342a27ddcd681c2507e61be7102c4673
SoufianeAziz <azizsoufiane04@gmail.com>
2023-06-16 08:50:17 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 /**
  * main - Entry point
  *
- *Description: A C program that prints with put function 
- *
- * Return: Always 0 (Success)
- */ 
-int main()
+ * Return: (0) with Success
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
