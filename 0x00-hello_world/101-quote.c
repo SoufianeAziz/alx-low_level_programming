b5837be1d1888c121523ff490a53beda2c4ada19
SoufianeAziz <azizsoufiane04@gmail.com>
2023-06-19 19:30:37 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    char q[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+
+    write(1, q, 59);
+
+    return (1);
+
+} 
