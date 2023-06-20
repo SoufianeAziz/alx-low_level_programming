f4be328a88d7cb113fccf46272b07b00542c8432
SoufianeAziz <azizsoufiane04@gmail.com>
2023-06-19 19:13:47 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0) with success
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 65; m <= 70; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
